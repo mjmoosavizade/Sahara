ecc6d5ed905c6e412b21544bda407c8c33b1f3f0
mjmoosavizade <mjmoosavizade@gmail.com>
2021-05-14 21:12:03 +0430

    edited admin display

--- categories/admin.py
+++ categories/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import Category
 
-admin.site.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'category_title', 'parent')
 
+admin.site.register(Category, CategoryAdmin)
--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from .models import Product
 
-admin.site.register(Product)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'product_name', 'category')
+
+admin.site.register(Product, ProductAdmin)
 
