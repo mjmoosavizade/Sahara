1a4608022278beb152a61b6cfa12b2af41480dad
ashkan mohammadi <mohammadiashkan1384@gmail.com>
2021-06-13 10:36:32 +0430

    feature: better search experience
    details:
    - open search window when user clicks the search-btn
    - close searhc window when user clicks the open area around the search window
    - add search_handler
    - add selectProduct function for code reusablity
    - add site icon to head section

--- sahara/static/js/main.js
+++ sahara/static/js/main.js
@@ -19,23 +19,32 @@ close.addEventListener("click", closeMenu);
 const search = (keyword) => {
     $.get("/search", { "keyword": keyword })
         .done((data) => {
+            toggle_search();
             $(".sl").css('display', 'flex');
-            data.results.forEach(element => {
-                $(".sl__plist-box").append(`<a href="javascript:void(0);" data="${element.slug}" class="sl__plist__pcard">
-                <div class="sl__plist__pcard-box">
-                    <div class="sl__plist__pcard-img"><img src="${element.product_photo}" alt="*">
+            $(".sl__plist-box").empty(); // clear previous search results
+            data.results.forEach((element, i) => {
+                let $cart = $(`<a href="javascript:void(0);" data="${element.slug}" class="sl__plist__pcard">
+                    <div class="sl__plist__pcard-box">
+                        <div class="sl__plist__pcard-img"><img src="${element.product_photo}" alt="*">
+                        </div>
+                        <div class="sl__plist__pcard-title">${element.product_name}</div>
                     </div>
-                    <div class="sl__plist__pcard-title">${element.product_name}</div>
-                </div>
-            </a>`)
+                </a>`);
+                $(".sl__plist-box").append($cart);
+                if (i == 0)
+                    selectProduct($cart[0]);
             });
             $(".sl__plist__pcard:first").addClass('active');
         })
         .fail(() => {
             console.log("fail")
         });
-
 };
+
+const toggle_search = () => {
+    $(".sl").fadeToggle(200);
+};
+
 const getProduct = slug => {
     return new Promise((resolve, reject) => {
         $.get("/get_product", { "slug": slug })
@@ -49,10 +58,63 @@ const getProduct = slug => {
     });
 
 };
-$('#navSearch').submit(function(e) {
-    e.preventDefault();
-    search($("#keyword").val())
-});
+
+function search_hanlder(event) {
+    event.preventDefault();
+    search($("#keyword").val());
+}
+
+function selectProduct(product_element){
+    $(product_element).addClass('active');
+    $(product_element).siblings().removeClass("active");
+    getProduct(product_element.getAttribute('data')).then( data =>{
+        stockClass = '';
+        originalClass = '';
+        if (data.type == 'Stock') {
+            stockClass = 'sl__pdetail__oos-original'
+            originalClass = 'sl__pdetail__oos-stock'
+        } else {
+            originalClass = 'sl__pdetail__oos-original'
+            stockClass = 'sl__pdetail__oos-stock'
+        }
+        $(".sl__pdetail-box").empty();
+        $(".sl__pdetail-box").append(`<div class="sl__pdetail__informations">
+                    <div class="sl__pdetail__informations-img">
+                        <img src="${data.product_photo}" alt="***">
+                    </div>
+                    <div class="sl__pdetail__informations-list">
+                        <div class="sl__pdetail__informations-title">${data.product_name}</div>
+                        <div class="sl__pdetail__informations-description">${data.brand}</div>
+                    </div>
+                </div>
+                <!-- Alternative Name -->
+                <div class="sl__pdetail__anl">
+                    <div class="sl__pdetail__anl-title">alternative name</div>
+                    <div class="sl__pdetail__anl-list">
+                        <div class="sl__pdetail__anl-item">${data.alternative_name_1}</div>
+                        <div class="sl__pdetail__anl-item">${data.alternative_name_2}</div>
+                        <div class="sl__pdetail__anl-item">${data.alternative_name_3}</div>
+                        <div class="sl__pdetail__anl-item">${data.alternative_name_4}</div>
+                    </div>
+                </div>
+                <!-- Origianl Or Stock -->
+                <div class="sl__pdetail__oos original">
+                    <div class="sl__pdetail__oos-box">
+                        <a href="#" class="${ stockClass }" >stock</a>
+                        <a href="#" class="${ originalClass }">original</a>
+                    </div>
+                </div>
+                <div class="sl__pdetail__price">
+                    <div class="sl__pdetail__price-title">قیمت</div>
+                    <div class="sl__pdetail__price-number">${data.price} ${data.currency}</div>
+                </div>`);
+    });
+}
+
+$('#navSearch').submit(search_hanlder);
+$('#search-btn').click(search_hanlder);
+$('.sl').click(search_hanlder);
+$('.sl-box').click(event=>event.stopPropagation());
 
 const addToCart = (slug, qty) => {
     cat = JSON.parse(localStorage.getItem('items'));
