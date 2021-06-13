9599b723d59cf87f7d401e1ba501e0eaf430363a
ashkan mohammadi <mohammadiashkan1384@gmail.com>
2021-06-13 15:06:28 +0430

    refactor: remove old code and one unneccessery variable decleration

--- categories/views.py
+++ categories/views.py
@@ -5,10 +5,6 @@ from products.models import Brand, Product
 
 def categories(request):
     categories_list = Category.objects.all()
-    selected_category = ""
-    # if 'category' in request.GET:
-    #     selected_category = request.GET.get('category')
-    #     products = Product.objects.filter(category__slug = selected_category)
     if (selected_category:=request.GET.get("category", False)):
         products = Product.objects.filter(category__slug = selected_category)
     else:
