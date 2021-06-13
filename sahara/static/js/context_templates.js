5af35b22e733f964ae400d99b69da2d68f9119cd
ashkan mohammadi <mohammadiashkan1384@gmail.com>
2021-06-13 19:51:49 +0430

    add: product details and loader template

--- sahara/static/js/context_templates.js
+++ sahara/static/js/context_templates.js
@@ -32,3 +32,43 @@ const T_product_notfound_box = `
     {msg}
 </div>
 `;
+
+// {product_photo} {product_name} {brand} {alternative_name[1-4]} {stock_class} {original_class}
+// {price} {currency}
+const T_product_details = `<div class="sl__pdetal__informations">
+<div class="sl__pdetail__informations-img">
+    <img src="{product_photo}" alt="***">
+</div>
+<div class="sl__pdetail__informations-list">
+    <div class="sl__pdetail__informations-title">{product_name}</div>
+    <div class="sl__pdetail__informations-description">{brand}</div>
+</div>
+</div>
+<!-- Alternative Name -->
+<div class="sl__pdetail__anl">
+<div class="sl__pdetail__anl-title">alternative name</div>
+<div class="sl__pdetail__anl-list">
+    <div class="sl__pdetail__anl-item">{alternative_name_1}</div>
+    <div class="sl__pdetail__anl-item">{alternative_name_2}</div>
+    <div class="sl__pdetail__anl-item">{alternative_name_3}</div>
+    <div class="sl__pdetail__anl-item">{alternative_name_4}</div>
+</div>
+</div>
+<!-- Origianl Or Stock -->
+<div class="sl__pdetail__oos original">
+<div class="sl__pdetail__oos-box">
+    <a href="#" class="{stock_class}" >stock</a>
+    <a href="#" class="{original_class}">original</a>
+</div>
+</div>
+<div class="sl__pdetail__price">
+<div class="sl__pdetail__price-title">قیمت</div>
+<div class="sl__pdetail__price-number">{price} {currency}</div>
+</div>`;
+
+
+const T_loader = `
+<div class="loader-dialog">
+    <div class="loader"></div>
+</div>
+`;
