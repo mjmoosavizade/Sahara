bc0863b8f283aaf2f2e90b9f06dfda767048b67b
ashkan mohammadi <mohammadiashkan1384@gmail.com>
2021-06-13 18:54:40 +0430

    fix: use replaceAll instead of replace in the formatter
    details:
    - add coments on `T_product_notfound_box`

--- sahara/static/js/context_templates.js
+++ sahara/static/js/context_templates.js
@@ -7,7 +7,7 @@ class TemplateFormatter {
     format(context) {
         for (const key in context) {
             console.log(`{${key}}`)
-            this.element_template = this.element_template.replace(`{${key}}`, context[key]);
+            this.element_template = this.element_template.replaceAll(`{${key}}`, context[key]);
         }
     }
 
@@ -19,13 +19,14 @@ class TemplateFormatter {
 // {slug} , {product_photo}, {product_name}
 const T_product_cart = `
 <a href="javascript:void(0);" data="{slug}" class="sl__plist__pcard">
-<div class="sl__plist__pcard-box">
-    <div class="sl__plist__pcard-img"><img src="{product_photo}" alt="*">
+    <div class="sl__plist__pcard-box">
+        <div class="sl__plist__pcard-img"><img src="{product_photo}" alt="*">
+        </div>
+        <div class="sl__plist__pcard-title">{product_name}</div>
     </div>
-    <div class="sl__plist__pcard-title">{product_name}</div>
-</div>
 </a>`;
 
+// {msg}
 const T_product_notfound_box = `
 <div style="color: black; padding: 10px 15px; border-radius: 15px; background-color: rgb(240, 240, 240);">
     {msg}
