da759717fa42857d60d24a03c5fd9f63ba671d64
ashkan mohammadi <mohammadiashkan1384@gmail.com>
2021-06-14 11:25:13 +0430

    feature: make filters functional on products page
    detail:
    - now using `num_products` to determine the number of products

--- categories/views.py
+++ categories/views.py
@@ -1,5 +1,7 @@
+from products.views import product
 from django.shortcuts import render
 from django.core.paginator import Paginator
+from django.utils.text import slugify
 from categories.models import Category
 from products.models import Brand, Product
 
@@ -9,6 +11,15 @@ def categories(request):
         products = Product.objects.filter(category__slug = selected_category)
     else:
         products = Product.objects.all()
+
+    if (filter_on:=request.GET.get("filter")):
+        filter_list = {
+            "newest": products.order_by("-date_added"),
+            "in-stock": products.filter(in_stock=True),
+            "most-visited": products.order_by("-reviews_count")
+        }
+        if filter_on in filter_list.keys():
+            products = filter_list[filter_on]
     brands = Brand.objects.filter(show_in_homepage = True)
     
 
@@ -21,6 +32,8 @@ def categories(request):
         'products' : paged_products,
         'brands' : brands,
         'current_category' : selected_category,
+        'filter_on': filter_on,
+        "num_products": len(products)
     }
 
     return render(request, 'categories/categories.html', context)
