86074beadf922017100ba12ce738f1308bcba399
ashkan mohammadi <mohammadiashkan1384@gmail.com>
2021-06-14 08:22:11 +0430

    chore: remove unneccessery imports

--- categories/views.py
+++ categories/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from django.core.paginator import Paginator
 from categories.models import Category
 from products.models import Brand, Product
 
